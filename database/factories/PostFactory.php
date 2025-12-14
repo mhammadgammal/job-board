20157401c5f62a3117be73c1a644720810ec6d02
mhammadgammal <mhammadgamal17@gmail.com>
2025-12-14 22:38:04 +0200

    Add factories for Comment, Post, and Tag models; implement default state definitions

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    protected $model = Post::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            'title' => $this->faker->title(),
+            'author' => $this->faker->name(),
+            'body' => $this->faker->paragraphs(3, true),
+            'published' => $this->faker->boolean(),
+        ];
+    }
+}
